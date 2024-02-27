55baa2c931c50b0ef9e4725661638a6cd02abea4
Abraão Sousa <abraao_andrade_sousa@hotmail.com>
2024-02-27 14:26:00 -0300

    error handling and style changes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // pegando o DOM
-
 const imagem = document.querySelector(".poster")
 let titulo = document.querySelector(".movie-title")
 const sinopse = document.querySelector(".sinopse")
@@ -8,17 +7,31 @@ const diretor = document.querySelector(".diretor")
 const rating = document.querySelector(".rating")
 const form = document.getElementById("form1")
 const formulario = document.getElementById("formulario")
-let testando = "Dead Poets Society"
+const hero = document.querySelector(".hero-container")
+const bttn = document.getElementsByTagName("submit")
+const popup = document.querySelector(".errpopup")
+let testando;
 
 function submitForm(event){
     event.preventDefault();
 
-    if (form.value.length !== 0 ){
+    if (form.value.length){
         testando = form.value;
         dadosFilme();
+        
     } else {
-        alert("Please Insert a Movie Title")
+        popup.textContent = "❌ Please insert a Movie name"
+        popup.style.visibility = "visible"
     }
+}
+
+function updateHTML(data){
+    rating.textContent = data.imdbRating + "⭐"
+    diretor.textContent = data.Director
+    genero.textContent = data.Genre
+    titulo.textContent = data.Title + " ("+data.Year + ")"
+    sinopse.textContent = data.Plot
+    imagem.src=data.Poster
 }
 
 async function dadosFilme() {  
@@ -28,15 +41,19 @@ async function dadosFilme() {
     const data = await response.json()
 
     // mudando o conteudo de texto
-    rating.textContent = data.imdbRating + "⭐"
-    diretor.textContent = data.Director
-    genero.textContent = data.Genre
-    titulo.textContent = data.Title + " ("+data.Year + ")"
-    sinopse.textContent = data.Plot
-    imagem.src=data.Poster
-
-
-    console.log(data)   
+     if (data.Response != "False"){
+        updateHTML(data);
+        setTimeout(() => {
+            hero.style.visibility="visible"
+            popup.style.visibility="hidden"
+        }, 100)
+     } else{
+        hero.style.visibility="hidden"
+        popup.style.visibility="visible"
+        popup.textContent = "❌ Movie not Found"
+     }
+     console.log(data)
 }
+
+
 // chamada da função
-dadosFilme();
